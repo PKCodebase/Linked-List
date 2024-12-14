f1c7003be89e84c8a1c742095e6fba96f21d3cc0
PKCodebase <kaushikprasad1659@gmail.com>
2024-12-14 08:19:32 +0530

    InsertFirst

---
+++ src/com/linked_list/InsertFirst.java
@@ -0,0 +1,31 @@
+package com.linked_list;
+
+public class InsertFirst {
+
+    private static Node insertFirst(Node head, int data) {
+        Node temp = new Node(data);
+        temp.next = head;
+        return temp;
+    }  static void displayList(Node head) {
+        Node currentNode = head;
+        while(currentNode != null) {
+            System.out.print(currentNode.data+"\t");
+            currentNode=currentNode.next;
+        }
+
+        System.out.println("\n");
+    }
+
+    public static void main(String[] args) {
+        Node head = null;
+        head = insertFirst(head, 10);
+        head = insertFirst(head, 20);
+        head = insertFirst(head, 30);
+        head = insertFirst(head, 40);
+        //System.out.println(head);
+
+        displayList(head);
+    }
+
+
+}
